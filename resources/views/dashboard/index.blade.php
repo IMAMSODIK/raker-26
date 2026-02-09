fa248e8de6d01bf4bd2fbe1f1d4feb9d11998b0e
IMAM SODIK <imamsodik00@gmail.com>
2026-02-09 09:34:21 +0700

    adf

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,10 +7,10 @@
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <a href="/export"
+            {{-- <a href="/export"
                 class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
                 <i class="fas fa-download fa-sm text-white-50"></i> Export Laporan
-            </a>
+            </a> --}}
         </div>
 
         <!-- Content Row -->
