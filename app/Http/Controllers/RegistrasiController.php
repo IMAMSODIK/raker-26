ad4260cb0b714a15d7a2ec17cb08df590a1bf406
IMAM SODIK <imamsodik00@gmail.com>
2026-02-10 08:32:13 +0700

    adf

--- app/Http/Controllers/RegistrasiController.php
+++ app/Http/Controllers/RegistrasiController.php
@@ -74,7 +74,7 @@ class RegistrasiController extends Controller
             ]);
         }
 
-        $data->absensi1 = 1;
+        $data->absensi2 = 1;
         $data->save();
 
         return response()->json([
--- resources/views/absensi.blade.php
+++ resources/views/absensi.blade.php
@@ -141,7 +141,7 @@
                                             value="{{ old('asal_instansi') }}" />
                                     </div>
                                     <button class="btn btn-primary shadow-none my-2 btn-submit"
-                                        type="button">Absen Hari 1</button>
+                                        type="button">Absen Hari ke-2</button>
                                 </form>
                             </div>
                         </div>
